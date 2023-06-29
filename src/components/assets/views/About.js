517fc8eaff41badaf26bce30688cefc97ce88b0f
Gerred Gruenspan <56643835+ggruenspan@users.noreply.github.com>
2023-06-28 20:34:17 -0400

    Added about & skills

--- src/components/assets/views/About.js
+++ src/components/assets/views/About.js
@@ -1,9 +1,28 @@
 import '../css/aboutStyle.css';
+// import me from '../images/me.jpg';
 
 export default function About() {
     return (
         <div class="about-container" id="about">
-            aaaaaaaaaaaaaaa
+            <div class="picture">
+                {/* <img src={me} alt="" class="center" height={550}/> */}
+            </div>
+            <div class="desc">
+                <p style={{fontSize: '36px'}}> <strong>I'm Gerred Gruenspan, an aspiring Front-End / Web / Software Developer from Ontario, Canada.</strong></p>
+                    <br></br>
+                <p style={{fontSize: '22px'}}> I possess a profound passion for taking on intricate challenges and converting them into refined 
+                    applications and visually captivating websites. The logical nature of coding and the intricacies 
+                    of program structures captivate me. My commitment lies in crafting efficient code across various 
+                    languages, including HTML, CSS, JavaScript, JAVA, C++, and SQL.
+                    <br></br><br></br>
+                    When I'm not immersed in coding or refining user experiences, I indulge in my other interests. 
+                    I find great pleasure in watching basketball & baseball games, exploring the latest automotive videos, 
+                    and even getting my hands dirty while fixing my own car whenever possible.
+                    <br></br><br></br>
+                    I am driven by the constant pursuit of improvement, and I continuously seek opportunities to 
+                    enhance my skills and expand my knowledge in the ever-evolving field of development.
+                </p>
+            </div>
         </div>
     );
 }
--- src/components/assets/views/Skills.js
+++ src/components/assets/views/Skills.js
@@ -1,9 +1,83 @@
+import React from 'react';
 import '../css/skillsStyle.css';
 
-export default function Skills() {
-    return ( 
-        <div class="skills-container" id="skills">
-            sssssssssssssssssssssss
+export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      hexSize: 80,
+      hexOrigin: {x: 100, y: 130}
+    }
+  }
+
+  componentWillMount() {
+    this.setState({
+      canvasSize: { canvasWidth: 800, canvasHeight: 700 }
+    })
+  }
+
+  componentDidMount() {
+    const {canvasWidth, canvasHeight} = this.state.canvasSize;
+    this.canvashex.width = canvasWidth;
+    this.canvashex.height = canvasHeight;
+    for (let i = 0; i < 5; i++) {
+      this.drawHex(this.canvashex, {x: 355, y: 100});
+      this.drawHex(this.canvashex, {x: 505, y: 100});
+      this.drawHex(this.canvashex, {x: 655, y: 100});
+      this.drawHex(this.canvashex, {x: 280, y: 235});
+      this.drawHex(this.canvashex, {x: 430, y: 235});
+      this.drawHex(this.canvashex, {x: 580, y: 235});
+      this.drawHex(this.canvashex, {x: 355, y: 368});
+      this.drawHex(this.canvashex, {x: 505, y: 368});
+      this.drawHex(this.canvashex, {x: 655, y: 368});
+    }
+  }
+
+  drawHex(canvasID, center) {
+    for (let i = 0; i <= 5; i++) {
+      let start = this.getHexCornerCord(center, i);
+      let end = this.getHexCornerCord(center, i + 1);
+      this.drawLine(canvasID, {x: start.x, y: start.y}, {x: end.x, y: end.y})
+    }
+  }
+
+  getHexCornerCord(center, i) {
+    let angle_deg = 60 * i - 30;
+    let angle_rad = Math.PI / 180 * angle_deg;
+    let x = center.x + this.state.hexSize * Math.cos(angle_rad);
+    let y = center.y + this.state.hexSize * Math.sin(angle_rad);
+    return this.Point(x, y);
+  }
+
+  Point(x, y) {
+    return{x: x, y: y};
+  }
+
+  drawLine(canvasID, start, end) {
+    const ctx = canvasID.getContext('2d');
+    ctx.beginPath();
+    ctx.moveTo(start.x, start.y);
+    ctx.lineTo(end.x, end.y);
+    ctx.stroke();
+    ctx.closePath();
+  }
+
+  drawHexCordinates(canvasID, center, h) {
+    const ctx = canvasID.getContext('2d');
+    ctx.fillText(h.q, center.x-10, center.y);
+    ctx.fillText(h.r, center.x+7, center.y);
+  }
+
+  render() {
+    return (
+      <div class="skills-container" id="skills">
+        <div class="title">
+          <span>Skills</span>
+          <h2>My Expertise</h2>
         </div>
-    );
+        <canvas ref={canvashex => this.canvashex = canvashex}></canvas>
+      </div>
+    )
+  }
 }
