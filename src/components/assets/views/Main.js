24bbdbd0c5024bd4ab81776044dfead330dcb27b
Gerred Gruenspan <ggruenspan@outlook.com>
2023-07-23 13:57:44 -0400

    Updated mobile styling

--- src/components/assets/views/Main.js
+++ src/components/assets/views/Main.js
@@ -1,10 +1,10 @@
 import Navbar from "../views/Navbar";
 import Home from "./Home";
-import About from "./About";
-import Skills from "./Skills";
-import Projects from "./Projects"
-import Experience from "./Experience";
-import Contact from "./Contact";
+// import About from "./About";
+// import Skills from "./Skills";
+// import Projects from "./Projects"
+// import Experience from "./Experience";
+// import Contact from "./Contact";
 // import Menu from "./Menu";
 // import ScrollStopper from '../js/ScrollStopper';
 
@@ -15,10 +15,10 @@ export default function Main() {
       <div style={{background: "#e9e7e9"}}>
         <Home />
         {/* <About /> */}
-        {/* <Skills />
-        <Experience />
-        <Projects />
-        <Contact />  */}
+        {/* <Skills /> */}
+        {/* <Experience /> */}
+        {/* <Projects /> */}
+        {/* <Contact />  */}
       </div>
     </>
   );
